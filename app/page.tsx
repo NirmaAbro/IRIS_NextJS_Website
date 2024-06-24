bebaad6af1bb71a9ed0fbd8d561ac6defbc1af58
Nirma Abro <NirmaAbro143@gmail.com>
2024-06-23 21:52:02 -0600

    nav bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import Login from "@/components/Login";
+// import Login from "@/components/Login";
 import Section2 from "@/components/Section2";
 import Section3 from "@/components/Section3";
 import Section4 from "@/components/Section4";
@@ -11,14 +11,14 @@ function Home() {
   return (
     <>
       <div className="sticky top-0 z-50">
-        <Login />
+        {/* <Login /> */}
         <Navbar />
       </div>
       <div>
-        <Hero />
-        <Section2 />
-        <Section3 />
-        <Section4 />
+        {/* <Hero /> */}
+        {/* <Section2 /> */}
+        {/* <Section3 /> */}
+        {/* <Section4 /> */}
       </div>
     </>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,15 +9,33 @@ function Navbar() {
   const [isServicesHovered, setIsServicesHovered] = useState(false);
   const [isAboutHovered, setIsAboutHovered] = useState(false);
   const [isopen, setisopen] = useState(false);
+  const [navHeight, setNavHeight] = useState('auto'); // state to handle navbar height
 
   const togglebutton = () => {
     setisopen(!isopen);
     console.log("btn clicked ");
   };
 
+  // Function to handle the height change
+  const handleAboutMouseEnter = () => {
+    setIsAboutHovered(true);
+    setNavHeight('200px'); // Increase the height when hovering "About Us"
+  };
+
+  const handleServicesMouseEnter = () => {
+    setIsServicesHovered(true);
+    setNavHeight('300px'); // Increase the height when hovering "Our Services"
+  };
+
+  const handleMouseLeave = () => {
+    setIsAboutHovered(false);
+    setIsServicesHovered(false);
+    setNavHeight('auto'); // Reset height when not hovering
+  };
+
   return (
-    <nav className="bg-black opacity-80 text-white p-4 z-50 sticky top-0 w-full">
-      <div className="mx-auto"> {/* Center content, removed px-4 for full width */}
+    <nav className="bg-black opacity-80 text-white p-4 z-50 sticky top-0 w-full transition-all duration-300" style={{ height: navHeight }}>
+      <div className="mx-auto">
         <div className="flex justify-between items-center">
           <div className="text-xl">
             <Image
@@ -42,21 +60,21 @@ function Navbar() {
             <div className="h-16 w-1 bg-white"></div>
             <div
               className="relative mt-9"
-              onMouseEnter={() => setIsAboutHovered(true)}
-              onMouseLeave={() => setIsAboutHovered(false)}
+              onMouseEnter={handleAboutMouseEnter}
+              onMouseLeave={handleMouseLeave}
             >
               <a href="#" className="hover:underline mt-9">
                 About Us
               </a>
               {isAboutHovered && (
                 <div className="absolute left-0 top-full mt-2 w-48 bg-black text-white shadow-lg rounded">
-                  <a href="#" className="block px-4 py-2 hover:bg-black">
+                  <a href="#" className="block px-4 py-0 hover:bg-black">
                     Our People
                   </a>
-                  <a href="#" className="block px-4 py-2 hover:bg-black">
+                  <a href="#" className="block px-4 py-0 hover:bg-black">
                     Join our Team
                   </a>
-                  <a href="#" className="block px-4 py-2 hover:bg-black">
+                  <a href="#" className="block px-4 py-0 hover:bg-black">
                     Distance learning Academy
                   </a>
                 </div>
@@ -66,31 +84,31 @@ function Navbar() {
             <div className="h-16 w-1 bg-white"></div>
             <div
               className="relative mt-9"
-              onMouseEnter={() => setIsServicesHovered(true)}
-              onMouseLeave={() => setIsServicesHovered(false)}
+              onMouseEnter={handleServicesMouseEnter}
+              onMouseLeave={handleMouseLeave}
             >
-              <a href="#" className="hover:underline">
+              <a href="#" className="hover:underline mt-9">
                 Our Services
               </a>
 
               {isServicesHovered && (
                 <div className="absolute left-0 top-full mt-2 w-48 bg-black text-white shadow-lg rounded">
-                  <a href="#" className="block px-4 py-2 hover:bg-black">
+                  <a href="#" className="block px-4 py-0 hover:bg-black">
                     Adult Courses
                   </a>
-                  <a href="#" className="block px-4 py-2 hover:bg-black">
+                  <a href="#" className="block px-4 py-0 hover:bg-black">
                     Apprenticeship
                   </a>
-                  <a href="#" className="block px-4 py-2 hover:bg-black">
+                  <a href="#" className="block px-4 py-0 hover:bg-black">
                     Commercial Training
                   </a>
-                  <a href="#" className="block px-4 py-2 hover:bg-black">
+                  <a href="#" className="block px-4 py-0 hover:bg-black">
                     Sector Base
                   </a>
-                  <a href="#" className="block px-4 py-2 hover:bg-black">
+                  <a href="#" className="block px-4 py-0 hover:bg-black">
                     Fully Funded Digital Skill Training
                   </a>
-                  <a href="#" className="block px-4 py-2 hover:bg-black">
+                  <a href="#" className="block px-4 py-0 hover:bg-black">
                     Carbon Literacy Training
                   </a>
                 </div>
