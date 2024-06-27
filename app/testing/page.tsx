2501dc07b29c343b2a1822b9740169858fb24a5f
Nirma Abro <NirmaAbro143@gmail.com>
2024-06-27 01:54:44 -0600

    testing folder added for test

---
+++ app/testing/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div className='bg-black'>
+            {/* // bg khudsy black dea ha q k text white ki waja sa text nhi dikhrha tha */}
+
+            <h1 className='text-red-600'>Mobile Screen</h1>
+            <h1 className='mobile-heading-one'>thinkfeet</h1>
+            <h2 className='mobile-heading-two'>thinkfeet</h2>
+            <h3 className='mobile-heading3span-twenty'>thinkfeet</h3>
+            <h3 className='mobile-headingthree-hero text-white'>thinkfeet</h3>
+            {/* //white apni taraf sa text color dea ha wrna dikhnhi rha tha color */}
+
+            <h1 className='text-red-600'>Tablet check</h1>
+            <h1 className='tablet-heading-one text-white'>thinkfeet</h1>
+            <h2 className='tablet-heading-two text-white'>thinkfeet</h2>
+            <h3 className='tablet-heading3span-twenty text-white'>thinkfeet</h3>
+            <h3 className='tablet-heading3-hero text-white'>thinkfeet</h3>
+
+
+
+            <h1 className='text-red-600 '>Desktop check</h1>
+            <h1 className='desktop-htwo-headingone text-white'>thinkfeet</h1>
+            <h2 className='desktop-Hsix-headingtwo text-white'>thinkfeet</h2>
+            <h3 className='desktop-heading3-twenty text-white'>thinkfeet</h3>
+            <h3 className='desktop-hsix-headingthree text-white'>thinkfeet</h3>
+
+
+        </div>
+    )
+}
+
+export default page
