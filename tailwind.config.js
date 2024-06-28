437628b58aa9e8f99050671b99c08da6bf5725dc
Nirma Abro <NirmaAbro143@gmail.com>
2024-06-27 21:20:26 -0600

    tailwind config changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,32 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#4285F4",
+        "on-primary": "#E4EDFD", 
+        "primary-container": "#022061",
+        "on-primary-container": "#FFFFFF",
+        "secondary": "#34A853",
+        "on-secondary": "#FFFFFF",
+        "secondary-container": "#71E086",
+        "on-secondary-container": "#007322",
+        "gray-one": "#F4F4F4",
+        "gray-two": "#D9D9D9",
+        "gray-three": "#BFBFBF",
+        "gray-four": "#A6A6A6",
+        "gray-five": "#8C8C8C",
+        "gray-six": "#404040",
+        "error-one": "#BA1A1A",
+        "error-two": "#FFDAD6",
+        "error-three": "#410002",
+        "green": "#14532d",
+        "yellow": "#FFC107",
+      },
+      fontFamily: {
+        font: "open sans"
+      }
+    },
   },
   plugins: [],
 }
